a6ce137dda2d82eb8fb42f1f04d7b331d959ac98
adambaldwin <adambaldwin9@gmail.com>
2021-01-28 16:19:57 +0000

    Initial commit

---
+++ app/src/main/java/com/example/notesappcleanarch/presentation/ListFragment.kt
@@ -0,0 +1,63 @@
+package com.example.notesappcleanarch.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.notesappcleanarch.R
+import com.example.notesappcleanarch.framework.ListViewModel
+import kotlinx.android.synthetic.main.fragment_list.*
+
+
+class ListFragment : Fragment() {
+
+    private val notesListAdapter = NotesListAdapter(arrayListOf())
+    private lateinit var viewModel: ListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        notesListView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = notesListAdapter
+        }
+
+        addNote.setOnClickListener { goToNoteDetails()}
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+        viewModel.notes.observe(this, Observer {notesList ->
+            loadingView.visibility = View.GONE
+            notesListView.visibility = View.VISIBLE
+            notesListAdapter.updateNotes(notesList.sortedByDescending { it.updateTime })
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getNotes()
+    }
+
+    private fun goToNoteDetails(id: Long = 0L){
+        val action: NavDirections = ListFragmentDirections.actionGoToNote(id)
+        Navigation.findNavController(notesListView).navigate(action)
+    }
+
+}
